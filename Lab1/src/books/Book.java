a5699265d4959a3d9056e2962f85ab1226e6432c
Pravesh <praveshpansari@gmail.com>
2019-10-18 18:09:07 +0545

    Additional Exercise1

--- Lab1/src/books/Book.java
+++ Lab1/src/books/Book.java
@@ -1,14 +1,20 @@
 package books;
 
+import java.time.LocalDate;
+
 public class Book {
 
-	private String title, author, publisher, copyrightDate;
-
-	public Book(String title, String author, String publisher, String copyrightDate) {
+	private String title, author, publisher;
+	LocalDate copyrightDate;
+	
+	public Book(String title, String author, String publisher, String copyrightString) {
 		this.title = title;
 		this.author = author;
 		this.publisher = publisher;
-		this.copyrightDate = copyrightDate;
+		String date[] = copyrightString.split("/");
+		int [] dateInt = new int[date.length];
+		for (int i=0;i<3;i++) dateInt[i] = Integer.parseInt(date[i]);
+		copyrightDate = LocalDate.of(dateInt[2],dateInt[1],dateInt[0]);
 	}
 
 	public String getTitle() {
@@ -35,17 +41,22 @@ public class Book {
 		this.publisher = publisher;
 	}
 
-	public String getCopyrightDate() {
+	public LocalDate getCopyrightDate() {
 		return copyrightDate;
 	}
-
-	public void setCopyrightDate(String copyrightDate) {
-		this.copyrightDate = copyrightDate;
+	
+	public String timePassed(LocalDate copyrightDate) {
+		LocalDate ld = LocalDate.now();
+		
+		int year = ld.getYear()-copyrightDate.getYear();
+		int month = Math.abs(ld.getMonthValue()-copyrightDate.getMonthValue());
+		int date = Math.abs(ld.getDayOfMonth()-copyrightDate.getDayOfMonth());
+		return String.format("%04d", year)+"-"+String.format("%02d", month)+"-"+String.format("%02d", date);
 	}
 
 	public String toString() {
 		return "Book title: " + getTitle() + "\nBook author: " + getAuthor() + "\nPublisher: " + getPublisher()
-				+ "\nCopyright Date: " + getCopyrightDate();
+				+ "\nCopyright Date: " + getCopyrightDate() + "\nTime Passed: " + timePassed(getCopyrightDate());
 	}
 
 }
--- Lab1/src/books/Bookshelf.java
+++ Lab1/src/books/Bookshelf.java
@@ -1,21 +1,57 @@
 package books;
 
+import java.lang.StringBuilder;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Bookshelf {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Book B1 = new Book("On the Origin of Species", "Charles Darwin", "John Murray", "24 November 1859");
-		Book B2 = new Book("A Brief History of Time", "Stephen Hawking", "Bantam Books", "1 March 1998");
-		Book B3 = new Book("Philosophiæ Naturalis Principia Mathematica", "Isaac Newton", "Benjamin Motte", "5 July 1687");
-		
+		Book[] Books = new Book[5];
+		int bookNum = 1;
+		StringBuilder sb = new StringBuilder();
+
+		Books[0] = new Book("On the Origin of Species", "Charles Darwin", "John Murray", "24/11/1859");
+		Books[1] = new Book("A Brief History of Time", "Stephen Hawking", "Bantam Books", "01/03/1998");
+		Books[2] = new Book("Philosophiæ Naturalis Principia Mathematica", "Isaac Newton", "Benjamin Motte",
+				"05/07/1687");
+		Books[3] = new Book("Pride and Prejudice", "Jane Austen", "Thomas Egerton", "28/01/1813");
+		Books[4] = new Book("Gulliver's Travels", "Jonathan Swift", "Benjamin Motte", "28/10/1726");
+
 		System.out.println("Books on the shelf: ");
 		System.out.println();
-		System.out.println("Book 1-\n"+B1);
-		System.out.println();
-		System.out.println("Book 2-\n"+B2);
-		System.out.println();
-		System.out.println("Book 3-\n"+B3);
-		
+		for (Book b : Books) {
+			System.out.println("Book " + bookNum + " -");
+			System.out.println(b);
+			System.out.println();
+			bookNum++;
+		}
+
+		File table = new File("table.html");
+		if (table.exists()) {
+			table.delete();
+			table.createNewFile();
+		} else {
+			table.createNewFile();
+		}
+		System.out.println("File Created");
+		FileWriter writer = new FileWriter("table.html");
+		writer.write(
+				"<html><head><style>caption{font-size:20pt;margin-bottom:20px;margin-top:40px;}th{background-color: #4CAF50;color: white;}th,td{padding: 15px;text-align: left;border-bottom: 1px solid #ddd;}tr:hover {background-color: #f5f5f5;}table{border-collapse: collapse;font-family: Calibri;font-size:large;}tr:nth-child(even) {background-color: #f2f2f2;}</style><title>BookShelf</title></head><body><table align=\"center\"><caption>Books</caption><thead><tr><th>Title</th><th>Author</th><th>Publisher</th><th>Copyright date</th></tr></thead><tbody>");
+
+		for (Book b : Books) {
+			sb.append("<tr>");
+			sb.append("<td>" + b.getTitle() + "</td>");
+			sb.append("<td>" + b.getAuthor() + "</td>");
+			sb.append("<td>" + b.getPublisher() + "</td>");
+			sb.append("<td>" + b.getCopyrightDate() + "</td>");
+			sb.append("</tr>");
+		}
+		writer.write(sb + "</tbody></table></body></html>");
+		writer.close();
+
 	}
 
 }
